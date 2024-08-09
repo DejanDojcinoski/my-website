5ebfad53df52d05fcdfa19cd35cc4dda0e1df333
Dejan Dojcinoski <donnie_brasco20@hotmail.com>
2024-08-09 09:02:08 +0200

    added text typing animation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 
+var typed = new Typed('.animation', {
+    strings: ["I'm a Software Developer", "I love sports"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+  });
+  
     
 
     
