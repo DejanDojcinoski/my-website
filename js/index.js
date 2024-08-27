bd3a9357cdf5f5a99ad49f52e8f30a8a1899dc36
Dejan Dojcinoski <donnie_brasco20@hotmail.com>
2024-08-27 21:10:14 +0200

    Certification section added

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 
 var typed = new Typed('.animation', {
-    strings: ["I'm a Software Developer", "I love sports"],
+    strings: ["I'm a Software Developer", "I love sports", "This website is under construction!!!"],
     typeSpeed: 60,
     backSpeed: 60,
     loop: true
   });
   
-    
-
-    
+  
